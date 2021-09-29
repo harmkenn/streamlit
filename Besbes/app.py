fd00422ae122fe8cd0766bcc64e67cdf7e6f13dc
Ken Harmon <harmkenn@gmail.com>
2021-09-29 16:40:34 -0600

    BESBES streamlit app

---
+++ Besbes/app.py
@@ -0,0 +1,22 @@
+# https://www.youtube.com/watch?v=wDysPcF0Hbc
+import streamlit as st 
+st.title("This is my first stremlit App")
+st.header("A machine learning application to detect types of flowers")
+st.subheader("built by Ken Harmon")
+st.markdown("this is a description")
+st.markdown("If True, the data is a pandas DataFrame including columns with appropriate dtypes (numeric). The target is a pandas DataFrame or Series depending on the number of target columns. If return_X_y is True, then (data, target) will be pandas DataFrames or Series as described below.")
+st.markdown("If True, the data is a pandas DataFrame including columns with appropriate dtypes (numeric). The target is a pandas DataFrame or Series depending on the number of target columns. If return_X_y is True, then (data, target) will be pandas DataFrames or Series as described below.")
+st.markdown("*Here is some italics*")
+st.markdown("**Here is some Bold**")
+st.markdown("* Here is bullet")
+st.info("Here is some information")
+st.warning("Here is a warning")
+st.error("Here is an error")
+
+
+
+
+
+
+
+
