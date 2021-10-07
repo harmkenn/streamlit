35f6772f1e03af43b3c25b6545390d65c60718f9
Ken Harmon <harmkenn@gmail.com>
2021-10-06 20:06:00 -0600

    upload my new multi page

---
+++ StatTools/app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from multiapp import MultiApp
+from apps import quant01, home, backends, basemaps, customize, demo, tile_layer, uber_nyc, vector
+
+st.set_page_config(layout="wide")
+
+
+apps = MultiApp()
+
+# Add all your application here
+apps.add_app("Quantitative Stats", quant01.app)
+apps.add_app("Home", home.app)
+apps.add_app("Create an interactive map", backends.app)
+apps.add_app("Customize the default map", customize.app)
+apps.add_app("Change basemaps", basemaps.app)
+apps.add_app("Add tile layers", tile_layer.app)
+apps.add_app("Add vector", vector.app)
+apps.add_app("Demo", demo.app)
+apps.add_app("Pydeck", uber_nyc.app)
+
+# The main app
+apps.run()
